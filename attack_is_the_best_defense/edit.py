0cb15f4b5eccbcda1cd057225739c9b6f242eae2
youssef-hassane <youssef_hassane@icloud.com>
2024-04-29 00:33:57 +0300

    Attack is the best defense - DONE

--- attack_is_the_best_defense/edit.py
+++
@@ -1,5 +0,0 @@
-with open("./rockyou.txt", "r") as r_file:
-    with open("./output.txt", "w") as w_file:
-        for line in r_file:
-            if len(line) == 12:
-                w_file.write(line)

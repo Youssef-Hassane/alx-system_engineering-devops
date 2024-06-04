11bad7a6da3426f2f99a1e325fa90c72302692f8
youssef-hassane <youssef_hassane@icloud.com>
2024-06-05 02:04:25 +0300

    Task #0 - DONE

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+""" the number of subscribers (not active users, total subscribers) for a given subreddit """
+import requests
+
+
+def number_of_subscribers(subreddit):
+    """
+        GET /r/subreddit/aboutread
+            Return information about the subreddit.
+            Data includes the subscriber count, description, and header image.
+    """
+    url = f"https://www.reddit.com/r/{subreddit}/about.json"
+    headers = {"User-Agent": "Mozilla/5.0"}
+    response = requests.get(url, headers=headers)
+
+    if response.status_code == 200:
+        data = response.json()
+        return data["data"]["subscribers"]
+    else:
+        return 0

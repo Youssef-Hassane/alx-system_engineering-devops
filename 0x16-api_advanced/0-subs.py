aaf274a3b39c7bb18c61a5f1c55b22a9c4738a83
youssef-hassane <youssef_hassane@icloud.com>
2024-06-05 02:15:13 +0300

    Task #0 - DONE

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,21 +1,23 @@
 #!/usr/bin/python3
-""" Exporting csv files"""
-import json
+"""
+the number of subscribers (not active users, total subscribers)
+for a given subreddit
+"""
 import requests
-import sys
 
 
 def number_of_subscribers(subreddit):
-    """Read reddit API and return number subscribers """
-    username = 'ledbag123'
-    password = 'Reddit72'
-    user_pass_dict = {'user': username, 'passwd': password, 'api_type': 'json'}
-    headers = {'user-agent': '/u/ledbag123 API Python for Holberton School'}
-    url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
-    client = requests.session()
-    client.headers = headers
-    r = client.get(url, allow_redirects=False)
-    if r.status_code == 200:
-        return (r.json()["data"]["subscribers"])
+    """
+        GET /r/subreddit/aboutread
+            Return information about the subreddit.
+            Data includes the subscriber count, description, and header image.
+    """
+    url = f"https://www.reddit.com/r/{subreddit}/about.json"
+    headers = {"User-Agent": "Custom"}
+    response = requests.get(url, headers=headers)
+
+    if response.status_code == 200:
+        data = response.json()
+        return data["data"]["subscribers"]
     else:
-        return (0)
+        return 0
